500b813b40785a6ef1891b890b13c13655ac47ea
Zhan Teyvans <teyvans@gmail.com>
2023-06-16 01:47:16 +0300

    Test added

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,40 @@
+package ru.netology.service;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
+public class CashbackHackServiceTest {
+    CashbackHackService service = new CashbackHackService();
+
+    @Test
+    public void AmountMax() {
+        assertEquals(service.remain(1000), 0);
+    }
+
+    @Test
+    public void AmountBelowMax() {
+//        assertEquals(service.remain(900), 100);
+        int amount = 900;
+        int expected = 100;
+        int actual = service.remain(amount);
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void AmountZero() {
+//        assertEquals(service.remain(0), 1000);
+        int amount = 0;
+        int expected = 1000;
+        int actual = service.remain(amount);
+        Assert.assertEquals(actual, expected);
+    }
+
+
+    @Test
+    public void AmountAboveMax() {
+        assertEquals(service.remain(1100), 900);
+    }
+
+}
