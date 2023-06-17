b6f1c2112608c0c6e2f9966384ee89322288d825
Zhan Teivans <118687635+JanTey@users.noreply.github.com>
2023-06-17 20:56:46 +0300

    Update unit5Test.java

--- src/test/java/ru/netology/service/unit5Test.java
+++ src/test/java/ru/netology/service/unit5Test.java
@@ -1,5 +1,7 @@
 package ru.netology.service;
 
+import org.junit.jupiter.api.Assertions;
+
 import org.junit.jupiter.api.Test;
 
 public class Unit5Test {
@@ -7,17 +9,16 @@ public class Unit5Test {
 
     @Test
     public void BoundaryValuesBelow() {
-        org.junit.Assert.assertEquals(1, service.remain(999));
+        Assertions.assertEquals(1, service.remain(999));
     }
 
     @Test
     public void AmountEqualZero() {
-        org.junit.Assert.assertEquals(1000, service.remain(0));
+        Assertions.assertEquals(1000, service.remain(0));
     }
 
     @Test
     public void BoundaryValuesAbove() {
-        org.junit.Assert.assertEquals(900, service.remain(1100));
+        Assertions.assertEquals(900, service.remain(1100));
     }
 }
-
